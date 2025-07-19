3fec50b7e347357a35ca16a40ef9a13645209c2a
wbelucky <39439193+WBelucky@users.noreply.github.com>
2025-07-19 18:05:36 +0900

    fix avante.lua

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -5,14 +5,17 @@ return {
   opts = {
     -- add any opts here
     -- for example
-    provider = 'openai',
-    openai = {
-      endpoint = 'https://api.openai.com/v1',
-      model = 'gpt-4o-mini-2024-07-18', -- your desired model (or use gpt-4o, etc.)
-      timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-      temperature = 0,
-      max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-      --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+    providers = {
+      openai = {
+        endpoint = 'https://api.openai.com/v1',
+        model = 'gpt-4o-mini-2024-07-18', -- your desired model (or use gpt-4o, etc.)
+        timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+        extra_request_body = {
+           temperature = 0,
+           max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+        },
+        --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+      },
     },
     -- system_prompt = function()
     --   local hub = require('mcphub').get_hub_instance()
